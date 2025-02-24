f50b4623efad5a8787cc62293622f3a73e833bfb
fernanddadantasm <fernandamoreira079@gmail.com>
2025-02-24 19:53:32 +0000

    Email e senha

---
+++ 3. exercicios/6. email_senha.py
@@ -0,0 +1,18 @@
+import os
+os.system("clear")
+
+#Elabore um algortimo
+
+
+usuário_cadastrado = "fernanda.dantas@hotmail.com"
+senha_cadastrada = "231068"
+
+login = str(input ("Login: "))
+senha = str(input( "Senha: "))
+
+
+
+if login == usuário_cadastrado and senha == senha_cadastrada:
+    print("Bem vindo")
+else:
+    print("Login ou senha inválidos!")
