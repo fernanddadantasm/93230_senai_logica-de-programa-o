16cfc87e24a3c6984d9d8587472b5f5ffb6e2a65
fernanddadantasm <fernandamoreira079@gmail.com>
2025-04-09 19:38:26 +0000

    Função def

--- 7. Funções/16. exercicio_calculo_imc.py
+++ 7. Funções/16. exercicio_calculo_imc.py
@@ -1,6 +1,6 @@
 import os 
 os.system("clear")
-
+#.
 #Escreva um programa que solicite ao utilizador o fornecimento do seu peso em kg 
 #e de sua altura em m e a partir deles calcule o índice de massa corpórea do utilizador.
 
