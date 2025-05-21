965f42987cec116f053fa1577c5d9aeb45513b3b
Fernanda Dantas <fernandamoreira079@gmail.com>
2025-05-21 18:30:57 +0000

    HTML / CSS

--- 7. Funções/6. exemplo_parametro.py
+++
@@ -1,8 +0,0 @@
-import os 
-os.system("clear")
-
-def saudacao(parametro):
-    print(f"Olá, {parametro}! Bem vindo(a) ao nosso site.")
-
-nome_visitante = (input("Digite seu nome: "))    
-saudacao(nome_visitante)
