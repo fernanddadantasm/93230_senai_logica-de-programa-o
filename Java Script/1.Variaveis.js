339ec6a31d5ef02e511526aa2ae939efc40c0563
Fernanda Dantas <fernandamoreira079@gmail.com>
2025-05-22 19:05:18 +0000

    Java Script

---
+++ Java Script/1.Variaveis.js
@@ -0,0 +1,15 @@
+//limpar terminais
+console.clear();
+
+// Variavéis
+let nome = "Lucas";
+// var nome = "Lucas"; // var é uma variável global, let é uma variável local
+
+nome = "Lucas Silva";
+
+// Exibir no console
+console.log(nome);
+console.log(`Nome: ${nome}`);
+
+
+
---
+++ Java Script/2.Constantes.js
@@ -0,0 +1,8 @@
+// Constantes
+
+const idade = 30;
+
+
+//Exibindo resultado
+console.log('Idade:', idade)
+console.log(`Idade:- ${idade}`)
