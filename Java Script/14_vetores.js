601e272362a355f7a6d6b86ad6a2bc765de0b0e7
Fernanda Dantas <fernandamoreira079@gmail.com>
2025-05-28 17:26:04 +0000

    JAVA SCRIPT

--- Java Script/14_vetores.js
+++ Java Script/14_vetores.js
@@ -5,5 +5,18 @@ console.log(frutas)
 // Exibindo o primeiro elemento do vetor
 console.log(frutas[0])
 // Exibindo o segundo elemento do vetor
-console.log(frutas[1])
+console.log(frutas[2])
 // Exibindo o terceiro elemento do vetor        
+
+frutas.push('Abacaxi') // Adicionando um elemento ao final do vetor
+console.log(frutas[2])
+
+frutas.pop() // Removendo o último elemento do vetor
+console.log(frutas)
+
+frutas.shift() // Removendo o primeiro elemento do vetor
+console.log(frutas)
+
+frutas.forEach((fruta, index) => {
+    console.log(`${index}: ${fruta}`) // Exibindo o índice e o valor de cada elemento do vetor
+})
