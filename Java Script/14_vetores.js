d4183b14cf16216d47047aed67a3e5b06dee78d1
Fernanda Dantas <fernandamoreira079@gmail.com>
2025-05-28 17:07:05 +0000

    JAVA SCRIPT

---
+++ Java Script/14_vetores.js
@@ -0,0 +1,9 @@
+// Criando um vetor 
+const frutas = ['Maça', 'Banana', 'Laranja', 'Uva', 'Pera']
+
+console.log(frutas)
+// Exibindo o primeiro elemento do vetor
+console.log(frutas[0])
+// Exibindo o segundo elemento do vetor
+console.log(frutas[1])
+// Exibindo o terceiro elemento do vetor        
